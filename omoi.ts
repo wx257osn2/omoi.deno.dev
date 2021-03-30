f3ac64ddf5b7020c3ff5fd84dfc85cd91447a108
I <wx257osn2@yahoo.co.jp>
2021-03-30 23:48:04 +0900

    initial commit

---
+++ omoi.ts
@@ -0,0 +1,11 @@
+addEventListener("fetch", (event) => {
+  event.respondWith(
+    new Response("思い出のデブ", {
+      status: 200,
+      headers: {
+        server: "denosr",
+        "content-type": "text/plain",
+      },
+    })
+  );
+});
